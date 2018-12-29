cb394001e4e7d4dd928226e072e398f9e59b3b25
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2018-12-29 17:29:45 -0200

    Instalando e configurando ejs como engine para as views

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(requisicao, resposta){
     resposta.send('<html><body><h1>Servidor rodando com express</h1></body></html>');
 });
