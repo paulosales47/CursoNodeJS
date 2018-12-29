3c076d615d6d241fb13481b8a3fe0c16dbedb34e
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2018-12-29 21:21:49 -0200

    Utilizando consign para realizar o autoload das rotas

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 let app = require('./config/server');
 
-require('./app/routes/noticias')(app);
-require('./app/routes/formulario_inclusao_noticia')(app);
-require('./app/routes/home')(app);
-
-
 app.listen(3000, function(){
     console.log('Servidor rodando com Express');
 })
--- config/server.js
+++ config/server.js
@@ -1,7 +1,10 @@
 let express = require('express');
+let consign = require('consign');
 let app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+consign().include('./app/routes').into(app);
 
 module.exports = app;
