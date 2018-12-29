92f129af14a10b04675f1e387dce98b193bf7021
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2018-12-29 17:26:48 -0200

    Respondendo com base na requisição - express

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ let app = express();
 
 app.get('/', function(requisicao, resposta){
     resposta.send('<html><body><h1>Servidor rodando com express</h1></body></html>');
-})
+});
+
+app.get('/tecnologia', function(requisicao, resposta){
+    resposta.send('<html><body><h1>PAGINA DE TECNOLOGIA</h1></body></html>');
+});
 
 app.listen(3000, function(){
     console.log('Servidor rodando com Express');
