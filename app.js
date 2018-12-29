3d77e75ad9f36b95bd30869e4890a00185545b8f
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2018-12-29 17:53:06 -0200

    Adicionando subpastas com as views e realizando configuração no app

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ let app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function(requisicao, resposta){
-    resposta.send('<html><body><h1>Servidor rodando com express</h1></body></html>');
+    resposta.render('home/render');
 });
 
 app.get('/tecnologia', function(requisicao, resposta){
     resposta.render('secao/tecnologia');
 });
+
+app.get('/formulario_inclusao_noticia', function(requisicao, resposta){
+    resposta.render('admin/form_add_noticia');
+});
+
+app.get('/noticia', function(requisicao, resposta){
+    resposta.render('noticias/noticias');
+});
+
 
 app.listen(3000, function(){
     console.log('Servidor rodando com Express');
