081138db4bbe0607f383f3a8035904f8b3697cc6
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2018-12-30 11:35:39 -0200

    Instalando e configurando body-parser para receber as informações do form

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,5 +2,9 @@ module.exports = function(app){
     app.get('/formulario_inclusao_noticia', function(requisicao, resposta){
         resposta.render('admin/form_add_noticia');
     });
-}
 
+    app.post('/Noticias/Salvar', function(requisicao, resposta){
+        let noticia = requisicao.body;
+        resposta.send(noticia);
+    });
+}
--- config/server.js
+++ config/server.js
@@ -1,9 +1,11 @@
 let express = require('express');
 let consign = require('consign');
 let app = express();
-
+let bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 consign()
     .include('./app/routes')
