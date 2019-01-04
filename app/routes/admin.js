9e45d27e29d6cf919ad007d5b31901c5793c3e93
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2019-01-03 22:27:55 -0200

    Implementando express-validator

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,10 +1,27 @@
+let {check, validationResult}  = require('express-validator/check');
+
 module.exports = function(aplicacao){
     aplicacao.get('/formulario_inclusao_noticia', function(requisicao, resposta){
         resposta.render('admin/form_add_noticia');
     });
 
-    aplicacao.post('/Noticias/Salvar', function(requisicao, resposta){
+    aplicacao.post('/Noticias/Salvar', [
+        check('TITULO', 'Titulo é obrigatório').not().isEmpty(),
+        check('RESUMO', 'Resumo é obrigatório').not().isEmpty(),
+        check('RESUMO', 'Resumo deve conter entre 10 e 100 caracteres').isLength(10, 100),
+        check('AUTOR', 'Autor é obrigatório').not().isEmpty(),
+        check('DT_NOTICIA', 'Data da notícia é obrigatório').not().isEmpty(),
+        check('NOTICIA', 'Notícia é obrigatório').not().isEmpty()
+    ],(requisicao, resposta) => {
         let noticia = requisicao.body;
+        let errosFormulario = validationResult(requisicao);
+        console.log(errosFormulario.isEmpty())
+        if(!errosFormulario.isEmpty())
+        {
+            resposta.render('admin/form_add_noticia');
+            return;
+        }
+
         let conexao = aplicacao.config.dbConnection();
         let noticiaDAO = new aplicacao.app.models.NoticiaDAO(conexao);
 
--- config/server.js
+++ config/server.js
@@ -2,12 +2,10 @@ let express = require('express');
 let consign = require('consign');
 let app = express();
 let bodyParser = require('body-parser');
-let expressValidator = require('express-validator');
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(expressValidator());
 
 consign()
     .include('./app/routes')
