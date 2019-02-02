1ccfa0a64c67c4837f5968a4b9be87c2292d0f3e
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2019-02-02 16:29:20 -0200

    Carregando a tela de noticias atraves do banco de dados

--- app/models/NoticiaDAO.js
+++ app/models/NoticiaDAO.js
@@ -6,7 +6,7 @@ class NoticiaDAO {
         this._conexao.query('SELECT * FROM portal_noticias.tb_noticias WHERE ID_NOTICIA = 1', callback);
     }
     BuscarNoticias(callback) {
-        this._conexao.query('SELECT * FROM portal_noticias.tb_noticias', callback);
+        this._conexao.query('SELECT * FROM portal_noticias.tb_noticias ORDER BY DT_CRIACAO DESC', callback);
     }
     SalvarNoticia(noticia, callback) {
         this._conexao.query('INSERT INTO TB_NOTICIAS SET ?', noticia, callback);
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,11 +1,11 @@
 module.exports = function(aplicacao){
 
     aplicacao.get('/noticia', function(requisicao, resposta){
-        aplicacao.app.controllers.noticia.noticia(aplicacao, resposta);
+        aplicacao.app.controllers.noticias.noticia(aplicacao, resposta);
     });
 
     aplicacao.get('/noticias', function(requisicao, resposta){
-        aplicacao.app.controllers.noticia.noticias(aplicacao, resposta);
+        aplicacao.app.controllers.noticias.noticias(aplicacao, resposta);
     });
 
 
