9538b82426feb71d58869d59b104e143d6e5bb1a
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2019-02-02 12:26:55 -0200

    Configurando acesso aos recursos da pasta public

--- config/server.js
+++ config/server.js
@@ -5,6 +5,9 @@ let bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
+
+
+app.use(express.static('./app/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 consign()
